baa5d5705834a3250480bf18688f00b6a08fdc71
rumiumi <shintamawarni76@mhs.pelitabangsa.ac.id>
2018-11-12 11:04:36 +0700

    first commit

--- Latihan2.cpp
+++ Latihan2.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  int n;
-  
-  cout << "Masukkan angka: ";
-  cin >> n;
-  
-  if ( n % 2 == 0)
-    cout << n << " adalah angka genap";
-  else
-    cout << n << " adalah angka ganjil";
-  
+//deklarasi fungsi
+int sum(int,int);
+
+float bagi(int a, int b) {
+    return a/b;
+}
+
+//fungsi utama
+int main(){
+
+  //memanggil fungsi
+  cout << sum(1,99) << endl;
+
+  int a, b;
+  cin >> a >> b;
+  cout << sum(a, b) << endl;
+
+  int c = sum(a, 10);
+  cout << c << endl;
+
+  cout << bagi(a,b) << endl;
+
   return 0;
+}
+
+/* fungsi didefinisikan setelah fungsi utama */
+int sum(int num1, int num2){
+    int num3 = num1+num2;
+    return num3;
 }
